b28492ec181074b5d1fa2afd46c6662484b95acb
Subham Pradhan <pradhansubham024@gmail.com>
2025-10-22 00:05:00 +0000

    make the background image more vissible (_for element <HeroSection>_)

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -22,7 +22,7 @@ export default function HeroSection() {
             fill
             className="object-cover opacity-20 dark:opacity-10"
           />
-          <div className="absolute inset-0 bg-black/30 dark:bg-black/50" />
+          <div className="absolute inset-0 bg-black/0 dark:bg-black/50" />
         </>
       )}
       <div className="container relative z-10 px-4 md:px-6">
